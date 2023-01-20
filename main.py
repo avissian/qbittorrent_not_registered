d63d204b7084712cde23f2523d0fc0f26ad35cbc
avissian <avissian@users.noreply.github.com>
2023-01-20 20:17:38 +0700

    Незарегестрированные раздачи ищутся через API (#3)
    * Получение статусов раздач через API

--- main.py
+++ main.py
@@ -2,181 +2,70 @@ import http.client as http_client
 import json
 import logging
 import os
-from urllib.parse import urlencode
+import time
 
 import qbittorrentapi
-import requests
+
+from api_rt import Rutracker
+from api_tlg import send_tlg
 
 debug = False
-api_url = "http://api.rutracker.org"
-forum_url = "https://rutracker.org"
 
 if debug:
     http_client.HTTPConnection.debuglevel = 1
 
-# noinspection PyUnresolvedReferences
-requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
+def process_torrent(torrent: qbittorrentapi.TorrentDictionary,
+                    tor_topic_data: dict,
+                    torrent_id: str,
+                    qbt_client: qbittorrentapi.Client,
+                    rutracker: Rutracker,
+                    forum_categories: dict,
+                    dry_run=False
+                    ):
+    msg = "%s\n\tPath: %s\n\tExternal id: %s" % (torrent.name, torrent.save_path, torrent_id)
+    logging.info(msg)
+    print(msg)
 
-def rutracker_auth(config):
-    headers = {"User-Agent": "Mozilla/5.0", "Content-Type": "application/x-www-form-urlencoded"}
+    file_name = rutracker.download_torrent(torrent_id)
 
-    payload = urlencode(
-        query={"login_username": config["rutracker"]["user"],
-               "login_password": config["rutracker"]["password"],
-               "login": "жопа"},
-        encoding="windows-1251")
-    session = requests.Session()
+    # Категория с торрента, или если нет - тема форума
+    category_name = torrent.get("category") or forum_categories.get(tor_topic_data.get("forum_id"))
 
-    if config.get("proxy"):
-        proxy = {"http": config["proxy"]}
-        proxy["https"] = proxy["http"]
-        session.proxies = proxy
-
-    session.verify = False
-
-    session.post(forum_url + "/forum/login.php", headers=headers, data=payload)
-    return session
-
-
-def download_torrent(torrent_id, session):
-    url = forum_url + "/forum/dl.php?t=" + torrent_id
-    headers = {"User-Agent": "Mozilla/5.0"}
-    resp = session.get(url, allow_redirects=True, headers=headers)
-    with open(torrent_id + ".torrent", "wb") as torr_file:
-        for chunk in resp.iter_content(chunk_size=1024):
-            torr_file.write(chunk)
-            torr_file.flush()
-            os.fsync(torr_file.fileno())
-
-
-def get_topic_data(external_torrent_id, session):
-    url = api_url + "/v1/get_tor_topic_data?by=topic_id&val=" + external_torrent_id
-    try:
-        resp = session.get(url, allow_redirects=True, verify=False)
-    except:
-        print("Cant process id: ", external_torrent_id)
-        return None
-    topic_data = json.loads(resp.content.decode("utf-8"))
-    try:
-        topic_data_id = topic_data["result"][external_torrent_id]["forum_id"]
-    except:
-        return None
-    return topic_data_id
-
-
-def get_torrent_cat(forum_id, session):
-    url = api_url + "/v1/get_forum_data?by=forum_id&val=" + str(forum_id)
-    try:
-        resp = session.get(url, allow_redirects=True, verify=False)
-    except:
-        print("Cant process id: ", forum_id)
-        return ""
-
-    category = json.loads(resp.content.decode("utf-8"))
-    category_name = category["result"][str(forum_id)]["forum_name"]
-    return category_name
-
-
-def process_torrent(torrent: qbittorrentapi.TorrentDictionary, qbt_client: qbittorrentapi.Client,
-                    session: requests.Session, config):
-    torrent_info = qbt_client.torrents_properties(torrent.hash)
-    torrent_external_id = torrent_info.comment.split("=")[-1]
-    print("\tPath:", torrent.save_path, "\n\tExternal id:", torrent_external_id)
-
-    download_torrent(torrent_external_id, session)
-    category_name = torrent.get("category")
-    if not category_name:
-        topic_data_id = get_topic_data(torrent_external_id, session)
-        if not topic_data_id:
-            return False
-        category_name = get_torrent_cat(topic_data_id, session)
-
-    if config["dry_run"]:
-        print("\t(dry run) add torrent: " + torrent.name)
+    if dry_run:
+        logging.warning(f"\t(dry run) add torrent: {torrent.name} "
+                        f"save_path={torrent.save_path} "
+                        f"category={category_name}")
     else:
-        ok = qbt_client.torrents_add(torrent_files=f"./{torrent_external_id}.torrent",
+        ok = qbt_client.torrents_add(torrent_files=file_name,
                                      save_path=torrent.save_path,
                                      category=category_name)
 
         if ok == "Ok.":
-            os.remove(f"./{torrent_external_id}.torrent")
+            os.remove(f"./{torrent_id}.torrent")
         else:
-            print(f"Ошибка добавления торрента {torrent_external_id}.torrent, статус ответа '{ok}'")
+            logging.warning(f"Ошибка добавления торрента {torrent_id}.torrent, статус ответа '{ok}'")
+            return False
     return True
-
-
-def check_torrent_registration(torrent: qbittorrentapi.TorrentDictionary, qbt_client: qbittorrentapi.Client,
-                               session: requests.Session):
-    if not torrent.get("infohash_v2"):
-        torrent_trackers_list = qbt_client.torrents_trackers(torrent.hash)
-        for torrent_info in torrent_trackers_list:
-            torrent_dict = dict(torrent_info)
-            logging.info(torrent_dict)
-            if torrent_dict["msg"] == "Torrent not registered":
-                print("Found unregistered torrent: ", torrent.name)
-                return torrent
-    else:  # гибридный торрент, проверим состояние по V1 hash вручную, потому что статус в кубите всегда ошибочный
-        # print("Hybrid: " + torrent.name + ", check by API")
-        return check_by_api(qbt_client, torrent, session)
-    return None
-
-
-def check_by_api(qbt_client: qbittorrentapi.Client, torrent: qbittorrentapi.TorrentDictionary,
-                 session: requests.Session):
-    torrent_info = qbt_client.torrents_properties(torrent.hash)
-    forum_id = torrent_info.comment.split("=")[-1]
-    url = api_url + "/v1/get_tor_hash?by=topic_id&val=" + forum_id
-    try:
-        resp = session.get(url, allow_redirects=True, verify=False)
-    except:
-        print("Cant process forum_id: ", forum_id)
-        return None
-
-    data = json.loads(resp.content.decode("utf-8"))
-    hash_v1 = torrent.infohash_v1.upper()
-    if hash_v1 != data["result"][forum_id].upper():
-        return torrent
-    return None
-
-
-def send_tlg_msg(from_bot, to_user, text: str):
-    """ Отправка текста в телеграм """
-    # пробуем отправить сообщение
-    params = {"chat_id": to_user,
-              "text": text}
-    if from_bot:
-        response = requests.get(f"https://api.telegram.org/bot{from_bot}/sendMessage", params)
-    else:
-        response = requests.get("https://bot.keeps.cyou/PlanB", params)
-
-    # обновляем файл с хешами только если отправилось сообщение
-    if response.status_code != 200:
-        print("Не удалось отправить сообщение в телеграм:", response.text)
-
-
-def send_tlg(from_bot, to_user, header_text, msg_set):
-    """ Отправка iterable в телеграм с разбивкой по длине до 4000 """
-    if len(msg_set) > 0 and to_user:
-        msg = header_text + "\n"
-        for item in msg_set:
-            if len(msg) + len(item) <= 4000:
-                msg += item + "\n"
-            else:
-                send_tlg_msg(from_bot, to_user, msg)
-                msg = item + "\n"
-        send_tlg_msg(from_bot, to_user, msg)
 
 
 def main():
     with open("config.json", "r") as f:
         config = json.load(f)
-    session = rutracker_auth(config)
+
+    rutracker = Rutracker(
+        user=config["rutracker"]["user"],
+        password=config["rutracker"]["password"],
+        api_key=config["rutracker"]["api_key"],
+        proxy=config.get("proxy"),
+        forum_url=config["rutracker"]["urls"]["forum"],
+        api_url=config["rutracker"]["urls"]["api"],
+        proxy_for_api=config.get("proxy_for_api"),
+    )
 
     for client in config["qbt"]["clients"]:
-        unregistered_files = []
-        added_files = []
-
+        new_torrents = []
+        bad_status = []
         qbt_client = qbittorrentapi.Client(
             host=client["host"],
             port=client["port"],
@@ -184,65 +73,120 @@ def main():
             password=client["password"],
             REQUESTS_ARGS={"timeout": (300, 300)}  # timeout (connect, read response)
         )
-        print("Processing client: ", client["host"], ":", client["port"])
+        msg = f"Подключение к клиенту: {client['host']}:{client['port']}"
+        logging.info(msg)
+        print(msg)
+
         try:
             qbt_client.auth_log_in()
         except qbittorrentapi.LoginFailed as e:
-            print(e)
-        print(f"qBittorrent: {qbt_client.app.version} Web API: {qbt_client.app.web_api_version}\n")
-        source_hashes = []
-        for torrent in qbt_client.torrents_info():
-            source_hashes.append(torrent.hash)  # для поиска добавленных хешей
-            unregistered = check_torrent_registration(torrent, qbt_client, session)
-            if unregistered:
-                ok = process_torrent(torrent, qbt_client, session, config)
-                if ok:
-                    if config["dry_run"]:
-                        print("\t(dry run) Removed old torrent: ", torrent.name)
-                    else:
-                        for file in qbt_client.torrents_files(torrent.hash):
-                            unregistered_files.append(file.name)
-                        qbt_client.torrents_delete(delete_files=False, torrent_hashes=torrent.hash)
-                        print("Removed old torrent: ", torrent.name)
+            logging.error(e)
+            continue
+        msg = f"Подключились, qBittorrent: {qbt_client.app.version} Web API: {qbt_client.app.web_api_version}"
+        logging.info(msg)
+        print(msg)
 
-        new_hashes = [x.hash for x in qbt_client.torrents_info() if x.hash not in source_hashes]
+        # получаем список раздач из кубита
+        print("Получаем список раздач")
+        torrents_info = qbt_client.torrents_info()
+        # чуть больше инфы по раздачам из кубита
+        print("Получение расширенной информации по раздачам")
+        torrents_prop = [qbt_client.torrents_properties(torrent.hash) for torrent in torrents_info]
 
-        for tor_hash in new_hashes:
-            for file in qbt_client.torrents_files(tor_hash):
-                added_files.append(file.name)
+        print("Обрабатываем данные")
+        # исходные хеши раздач (для поиска новых и удалённых раздач)
+        source_hashes = [x.hash for x in torrents_info]
+        # вытащим ID раздач форума из коммента
+        torrents_ids = [x.comment.split("=")[-1] for x in torrents_prop]
 
-        added_list = set(added_files) - set(unregistered_files)
-        print("Добавленные файлы:", "\n\t" + "\n\t".join(added_list))
+        print("Получаем из API данные по раздачам")
+        tor_topic_data = rutracker.get_tor_topic_data(torrents_ids)
 
-        orphans_list = set(unregistered_files) - set(added_files)
-        if len(orphans_list) > 0:
-            print("Файлы без торрентов:", "\n\t" + "\n\t".join(orphans_list))
+        print("Обрабатываем данные от API")
+        # Список всех forum_id
+        forum_ids = list(set(str(tor_topic_data.get(x, {}).get("forum_id")) for x in tor_topic_data
+                             if tor_topic_data.get(x) and tor_topic_data.get(x, {}).get("forum_id")))
+        # Категории с форума
+        print("Получаем из API данные по категориям")
+        forum_categories = rutracker.get_forum_data(forum_ids)
+
+        print("Ищем обновившиеся раздачи")
+        for idx, torrent in enumerate(torrents_info):
+            torrent_id = torrents_ids[idx]
+            tor_api_data = tor_topic_data.get(torrent_id)
+
+            if tor_api_data:
+                # Перекачиваем только для статусов по списку
+                if tor_api_data.get("tor_status") in [0, 2, 3, 4, 8, 9, 10]:
+                    if tor_api_data.get('info_hash') != torrent.infohash_v1.upper():
+                        logging.debug("Хеш раздачи изменился, перекачаем")
+
+                        new_torrents.append(torrent.name)
+
+                        ok = process_torrent(torrent=torrent,
+                                             torrent_id=torrent_id,
+                                             tor_topic_data=tor_api_data,
+                                             qbt_client=qbt_client,
+                                             rutracker=rutracker,
+                                             forum_categories=forum_categories,
+                                             dry_run=config["dry_run"],
+                                             )
+                        if ok:
+                            if config["dry_run"]:
+                                logging.warning("\t(dry run) Removed old torrent: %s" % (torrent.name,))
+                            else:
+                                qbt_client.torrents_delete(delete_files=False, torrent_hashes=torrent.hash)
+                                msg = f"Removed old torrent: {torrent.name}"
+                                logging.info(msg)
+                                print(msg)
+                else:
+                    logging.debug(f'Статус торрента {tor_api_data.get("tor_status")} - '
+                                  f'"{rutracker.statuses.get(int(tor_api_data.get("tor_status")))}"'
+                                  f' имя: {torrent.name}')
+                    bad_status.append(f'{tor_api_data.get("tor_status")} - '
+                                      f'"{rutracker.statuses.get(int(tor_api_data.get("tor_status")))}"'
+                                      f' имя: {torrent.name}')
+            else:
+                msg = f'Торрент не найден в ответе API: {torrent.name}'
+                logging.info(msg)
+                print(msg)
 
         # уведомление в телеграм
-        if config.get("telegram", {}).get("receiver_user_id"):
+        cfg_telegram = config.get("telegram")
+        if cfg_telegram and cfg_telegram.get("receiver_user_id"):
             client_name = client["host"] + ":" + str(client["port"])
-            if len(added_list) > 0 and config["telegram"].get("notice_added_files"):
+            if len(new_torrents):
                 send_tlg(
-                    config["telegram"].get("sender_bot_token"),
-                    config["telegram"].get("receiver_user_id"),
-                    client_name + ": Добавленные файлы:",
-                    added_list
+                    cfg_telegram.get("sender_bot_token"),
+                    cfg_telegram.get("receiver_user_id"),
+                    client_name + ": Перекачанные раздачи:",
+                    new_torrents
                 )
-            if len(orphans_list) > 0 and config["telegram"].get("notice_orphaned_files"):
+            if len(bad_status) > 0:
                 send_tlg(
-                    config["telegram"].get("sender_bot_token"),
-                    config["telegram"].get("receiver_user_id"),
-                    client_name + ": Файлы без торрентов:",
-                    orphans_list
+                    cfg_telegram.get("sender_bot_token"),
+                    cfg_telegram.get("receiver_user_id"),
+                    client_name + ': "Плохие" статусы раздач:',
+                    bad_status
                 )
+
+    logging.debug("Выход")
 
 
 if __name__ == "__main__":
-    if debug:
-        logging.basicConfig()
-        logging.getLogger().setLevel(logging.DEBUG)
-        requests_log = logging.getLogger("requests.packages.urllib3")
-        requests_log.setLevel(logging.DEBUG)
-        requests_log.propagate = True
+    try:
+        os.mkdir("./logs")
+    except FileExistsError:
+        pass
+
+    logger = logging.getLogger()
+
+    logger.setLevel(logging.DEBUG)
+
+    f_handler = logging.FileHandler("./logs/" + time.strftime("%y%m%d-%H%M.log"), encoding="utf-8")
+    f_formatter = logging.Formatter(u"%(filename)-.10s[Ln:%(lineno)-3d]%(levelname)-8s[%(asctime)s]|%(message)s")
+    f_handler.setFormatter(f_formatter)
+    f_handler.setLevel(logging.DEBUG)
+    logger.addHandler(f_handler)
     #
     main()
